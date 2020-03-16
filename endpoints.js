c02d868730e24f3be2709ea0c22b87580ad7f3fd
MortenDyberg <dyberg@live.dk>
2020-03-16 13:49:51 +0100

    Oprettelse af server og endpoint for forside

--- endpoints.js
+++ endpoints.js
@@ -8,9 +8,15 @@ app.listen(3000, ()=>{
     console.log("App listening on port 3000")
 });
 
-
+//html fil for forsiden bliver vist
 app.get('/',(req, res)=>{
     res.sendFile(path.resolve(__dirname, 'html/index.html'))
 });
+
+//html fil for login side bliver vist
+app.get('/Loginpage', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'html/Loginpage.html'))
+});
+
 
 
