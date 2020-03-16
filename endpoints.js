7a81091a7fb2e0586a10162483de73c1b6a6d937
Mikkel0608 <mikkel_marcher@hotmail.com>
2020-03-16 14:02:52 +0100

    redirects

--- endpoints.js
+++ endpoints.js
@@ -14,8 +14,35 @@ app.get('/',(req, res)=>{
 });
 
 //html fil for login side bliver vist
-app.get('/Loginpage', (req, res)=>{
+app.get('/Loginpage.html', (req, res)=>{
     res.sendFile(path.resolve(__dirname, 'html/Loginpage.html'))
+});
+app.get('/index.html',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'public/html/index.html'))
+});
+app.get('/orderPage.html',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'public/html/orderPage.html'))
+});
+app.get('/profile.html',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'public/html/profile.html'))
+});
+app.get('/orderConfirmation.html',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'public/html/orderConfirmation.html'))
+});
+app.get('/Register2.html',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'public/html/Register2.html'))
+});
+app.get('/Changeuser.html',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'public/html/Changeuser.html'))
+});
+app.get('/Changeorder.html',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'public/html/Changeorder.html'))
+});
+app.get('/Adminpage.html',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'public/html/Adminpage.html'))
+});
+app.get('/Calendar.html',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'public/html/Calendar.html'))
 });
 
 
