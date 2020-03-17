d709e2387cb474c33a8afe4309f84385bdc1790e
MortenDyberg <dyberg@live.dk>
2020-03-17 13:31:41 +0100

    Rettet primary keys

--- Models/db.js
+++ Models/db.js
@@ -13,7 +13,7 @@ module.exports = pool;
 const createTables = () =>{
     const queryText = `CREATE TABLE IF NOT EXISTS
                         Customers(
-                            customerId INT PRIMARY KEY,
+                            customerId SERIAL PRIMARY KEY,
                             customerName VARCHAR(50),
                             streetName VARCHAR(50),
                             streetNumber INT,
@@ -25,14 +25,14 @@ const createTables = () =>{
                             
                             CREATE TABLE IF NOT EXISTS
                             Products(
-                            productId INT PRIMARY KEY,
+                            productId SERIAL PRIMARY KEY,
                             price INT,
                             modelName VARCHAR(50)
                             );
                             
                             CREATE TABLE IF NOT EXISTS
                             Orders(
-                            orderId INT PRIMARY KEY,
+                            orderId SERIAL PRIMARY KEY,
                             orderDay VARCHAR(50),
                             orderMonth VARCHAR(50),
                             orderYear INT,
@@ -41,11 +41,11 @@ const createTables = () =>{
                             
                             CREATE TABLE IF NOT EXISTS 
                             orderProduct(
-                            orderProductId INT PRIMARY KEY);
+                            orderProductId SERIAL PRIMARY KEY);
                             
                             CREATE TABLE IF NOT EXISTS
                             admin(
-                            adminId INT PRIMARY KEY,
+                            adminId SERIAL PRIMARY KEY,
                             userName VARCHAR(50),
                             password VARCHAR(50));`;
 
