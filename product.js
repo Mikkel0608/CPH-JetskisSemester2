df81114736832878707d0fb1e27a87f7bb270af3
Benhn12 <ben.hn12@gmail.com>
2020-03-20 15:53:52 +0100

    Imports lavet i product.js

--- product.js
+++ product.js
@@ -1,4 +1,6 @@
+// import: Express-session
 const session = require('express-session');
+// Import: bodyParser
 const bodyParser = require('body-parser');
 
 // Import: DB
@@ -6,3 +8,13 @@ const pool = require('./Models/db');
 //Import: Express server
 const app = require('./index.js');
 
+// TODO Hvad gør dette stykke kode?
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+//the bodyParser extracts the form data from our orderPage.html file.
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
