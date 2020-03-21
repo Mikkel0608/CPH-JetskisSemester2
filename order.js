7aa6cf784a1aa1604bca050a427cc625554c3887
Mikkel0608 <mikkel_marcher@hotmail.com>
2020-03-21 22:15:26 +0100

    updated order.js

--- order.js
+++ order.js
@@ -13,7 +13,7 @@ app.use(session({
     saveUninitialized: true
 }));
 
-//the bodyParser extracts the form data from our orderPage.html file.
+//the bodyParser extracts the form data from our html file.
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
@@ -26,10 +26,47 @@ module.exports = function (request, response) {
     var orderAmount2 = request.body.orderAmount2;
     var orderAmount3 = request.body.orderAmount3;
     var totalPrice = request.body.totalPriceHidden;
-    console.log([rentDay, rentMonth, rentYear, rentTime, orderAmount1, orderAmount2, orderAmount3, totalPrice]);
-    console.log(request.session.phone);
+
+    //If no phone is registered, an error is shown:
     if (request.session.phone === undefined) {
-        response.send("Session timed out. Please login again and resubmit your order. <br><br><br> <a href='/loginpage.html'> Click here to go to the loginpage</a>");
+        response.send("Session timed out. Please login again and resubmit your order. <br><br><br> <a href='/loginpage.html'>Click here to go to the login page</a>");
+    }
+
+    // The following section uses the phone stored in the session to find the corresponding customerid in the database
+    var customerID = null;
+    pool.query(`SELECT customerid FROM customers WHERE phone =$1`, [request.session.phone], function (error, results) {
+        if(error) {
+            throw error;
+        } else {
+            customerID = results.rows[0].customerid;
+            createOrder();
+        }
+    });
+    /* Denne kode skal nok ikke bruges, ser ud til at virke uden
+    function setIdResult(foundID) {
+        customerID = foundID;
+        console.log([rentDay, rentMonth, rentYear, rentTime, orderAmount1, orderAmount2, orderAmount3, totalPrice, customerID]);
+        createOrder();
+
+    }
+
+     */
+    //Creates the order row in the DB from the collected information
+    function createOrder() {
+        pool.query(`INSERT INTO orders(
+        customerid, 
+        orderday, 
+        ordermonth, 
+        orderyear, 
+        timeperiod, 
+        orderprice) 
+        VALUES($1, $2, $3, $4, $5, $6);
+        `, [customerID, rentDay, rentMonth, rentYear, rentTime, totalPrice]);
+        createLineItems()
+    }
+
+    function createLineItems() {
+
     }
 
 
