0fae5673eb08875c7012ccdd0cca20a783eecf73
Mikkel0608 <mikkel_marcher@hotmail.com>
2020-04-24 17:31:14 +0200

    added ability to see all orders on admin

--- views/Javascript/admin_orders.js
+++ views/Javascript/admin_orders.js
@@ -9,12 +9,44 @@ const price = document.getElementById('price');
 price.value = 3;
 
 
-sortBtn.onclick = ()=>{
+sortBtn.onclick = () => {
     fetch(`/adminpage/allOrders/${sort.value}`, {
         method: 'GET',
     }).then(response => response.json())
         .then(json => {
             console.log(json);
+            var fetchedOrders = json;
+            var node = document.getElementById("orderList");
+            while (node.firstChild) {
+                node.removeChild(node.lastChild);
+            }
+            for (let i = 0; i < fetchedOrders.length; i++) {
+
+                var day = fetchedOrders[i].orderday;
+                var month = fetchedOrders[i].ordermonth;
+                var year = fetchedOrders[i].orderyear;
+                var timePeriod = fetchedOrders[i].timeperiod;
+                var orderPrice = fetchedOrders[i].orderprice;
+                var orderID = fetchedOrders[i].orderid;
+                var userID = fetchedOrders[i].userid;
+                var orderDate = fetchedOrders[i].order_placed_at;
+                var orderProducts = fetchedOrders[i].products;
+
+                var order = document.createElement("P");
+                order.innerHTML = "<b> Ordre ID: " + orderID + "</b></br></br><b> Bruger ID</b>: " + userID +  "</br></br><b>Dato for udlejning: </b>" + day + "/" + month + "/" + year + "</br></br>" + "<b>Tidspunkt for udlejning: kl. </b>" + timePeriod + "</br></br>" /*+ product + "</br></br"*/ + "<b>Pris total: </b> " + orderPrice + "</br></br> <b> Ordre lavet d. : </b>" + orderDate;
+                node.appendChild(order);
+
+                var product = null;
+                for (let x = 0; x < orderProducts.length; x++) {
+                    product = document.createElement('p');
+                    product.innerHTML = '<b>Produkt: </b>' + orderProducts[x].modelname + '<br>' +
+                        '<b>Antal: </b>' + orderProducts[x].count + '<br>' +
+                        '<b>Pris: </b>' + orderProducts[x].price * orderProducts[x].count;
+                    node.appendChild(product);
+                }
+                var separator = document.createElement('hr');
+                node.appendChild(separator);
+            }
         })
 };
 
